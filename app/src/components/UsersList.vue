720124533fab8a1f7f13e1721c1941ce8a3f5bc4
Lawrence Naumov <prostolawr@gmail.com>
2023-03-26 14:24:28 +0800

    Edit

--- app/src/components/UsersList.vue
+++ app/src/components/UsersList.vue
@@ -1,49 +1,52 @@
 <template>
-  <div>
-	<ul>
-		<li v-for="user in users" :key="user.name">
-			<p>
-			{{ user.name }}
-			{{ user.job_title }}
-			{{ user.coordinates}}
-			</p>
-		</li>
-	</ul>
-  </div>
+    <div>
+        <ul>
+            <li v-for="user in users" :key="user.name">
+                <p>
+                    {{ user.name }}
+                    {{ user.job_title }}
+                    {{ user.coordinates }}
+                </p>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'UsersList',
-  data() {
-	return
-  },
-  computed: {
-	users() {
-		return this.$store.state.users;
-	}
-  },
-  mounted() {
-	this.$store.dispatch("fetchUsers");
-  }
-  }
+    name: 'UsersList',
+    data() {
+        return
+    },
+    computed: {
+        users() {
+            return this.$store.state.users;
+        }
+    },
+    mounted() {
+        this.$store.dispatch("fetchUsers");
+    }
+}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
+
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
+
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,38 +1,36 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+import axios from "axios"
+
 
 export default createStore({
-  state: {
-	  users: []
-  },
-  getters: {
-  },
-  mutations: {
-    setProductData(state, userData) {
-	  state.users = userData.map(user => {
-	    return {
-	      name: user.name,
-	      job_title: user.job_title,
-	      coordinates: user.coordinates,
-	    };
-	  });
-	}
+    state: {
+        users: []
+    },
+    getters: {},
+    mutations: {
+        setProductData(state, userData) {
+            state.users = userData.map(user => {
+                return {
+                    name: user.name,
+                    job_title: user.job_title,
+                    coordinates: user.coordinates,
+                };
+            });
+        }
 
-  },
-  actions: {
-	fetchProducts({ commit }) {
-	  const baseURL = "https://ar.lyceumland.ru/api/user";
-	  axios
-	    .get(baseURL)
-	    .then(response => {
-	      commit("setUserData", res.data);
-	    })
-	    .catch(e => {
-	      console.log(e); 
-	    });
-	  });
-	}
-
-  },
-  modules: {
-  }
+    },
+    actions: {
+        fetchProducts({commit}) {
+            const baseURL = "https://ar.lyceumland.ru/api/user";
+            axios
+                .get(baseURL)
+                .then(response => {
+                    commit("setUserData", response.data);
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        }
+    },
+    modules: {}
 })
