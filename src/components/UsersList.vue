194062c9e0e079663926621c90122e3cbe93bf84
romakot321 <play62342@gmail.com>
2023-03-26 14:16:36 +0800

    Add userslist

---
+++ src/components/UsersList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+	<ul>
+		<li v-for="user in users" :key="user.name">
+			<p>
+			{{ user.name }}
+			{{ user.job_title }}
+			{{ user.coordinates}}
+			</p>
+		</li>
+	</ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UsersList',
+  data() {
+	return
+  },
+  computed: {
+	users() {
+		return this.$store.state.users;
+	}
+  },
+  mounted() {
+	this.$store.dispatch("fetchUsers");
+  }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,36 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+	  users: []
   },
   getters: {
   },
   mutations: {
+    setProductData(state, userData) {
+	  state.users = userData.map(user => {
+	    return {
+	      name: user.name,
+	      job_title: user.job_title,
+	      coordinates: user.coordinates,
+	    };
+	  });
+	}
+
   },
   actions: {
+	fetchProducts({ commit }) {
+	  const baseURL = "https://ar.lyceumland.ru/api/user";
+	  axios
+	    .get(baseURL)
+	    .then(response => {
+	      commit("setUserData", res.data);
+	    })
+	    .catch(e => {
+	      console.log(e); 
+	    });
+	  });
+	}
+
   },
   modules: {
   }
